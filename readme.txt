ee5f37a74851a875d5dd128a8596dad5b032ce96
zhangka001 <429590359@qq.com>
2016-09-19 14:37:19 +0800

    111

---
+++ readme.txt
@@ -0,0 +1,3 @@
+1. change database's password,username and db's name in generatorConfig.xml
+2. change your directory which you want to ganerate 
+3. run com.jack.mybatis.mbg java program
---
+++ src/com/jack/mybatis/test/UserMapperTest.java
@@ -0,0 +1,34 @@
+package com.jack.mybatis.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserMapperTest
+{
+
+	@Test
+	public void testDeleteByPrimaryKey()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInsert()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSelectByPrimaryKey()
+	{
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdateByPrimaryKey()
+	{
+		fail("Not yet implemented");
+	}
+
+}
